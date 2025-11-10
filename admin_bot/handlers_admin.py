998c79b4196cb49931ad9137764061fb71aa62c3
joaoemanoelaho <joaoemanoelaho@gmail.com>
2025-11-10 13:38:18 -0300

    Ajusta a prioridade na atribuição do nome do arquivo para considerar primeiro a legenda do post

--- admin_bot/handlers_admin.py
+++ admin_bot/handlers_admin.py
@@ -570,7 +570,7 @@ async def new_series_in_channel_handler(update: Update, context: ContextTypes.DE
         return
     status_msg = None
     try:
-        file_name = post.video.file_name or post.caption
+        file_name = post.caption or post.video.file_name
         if not file_name: return
         
         file_id = post.video.file_id
